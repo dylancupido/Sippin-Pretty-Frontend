3c5f5b13885c6d25aadfe51136f1b8d5edbef464
Mulanga radzilani <mulangaradzilani@gmail.com>
2025-05-23 08:10:05 +0200

    registeradd

--- src/Components/RegisterPage.jsx
+++ src/Components/RegisterPage.jsx
@@ -1,31 +1,107 @@
-import React from "react";
-import { FaUser,FaLock } from "react-icons/fa";
-import"../Styles/Register.css"
+import React, { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import "../Styles/Register.css";
+import axios from "axios";
 
-const CusRegisterForm=()=>{
-    return (
-        <div className="Wrapper">
-            <form action="">
-                <h1>Create Account</h1>
-                <div className="Inputbox">
-                <input type="text" placeholder="First Name*" require/>
-                </div>
-                <div className="Inputbox">
-                    <input type="text" placeholder="Last Name*"/>
-                </div>
-                <div className="Inputbox">
-                    <input type="text" placeholder="Email Address*"required/>
-                </div>
-                <div className="Inputbox">
-                    <input type="text" placeholder="Mobile Number*" required/>
-                </div>
-                <div className="Inputbox">
-                    <input type="Password" placeholder="Password*" required/>
-                </div>
-                <button>Register</button>               
-            </form>
+const CusRegisterForm = () => {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [phonenumbers, setPhonenumbers] = useState('');
 
+  const handleFirstnameChange = (e) => setFirstname(e.target.value);
+  const handleLastnameChange = (e) => setLastname(e.target.value);
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handlePhonenumbersChange = (e) => setPhonenumbers(e.target.value);
+
+  const handleOnClick = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Firstname: firstname,
+      Lastname: lastname,
+      PhoneNumbers: phonenumbers,
+      Password: password,
+    };
+
+    const url = 'https://localhost:44379/Sippin/Registration';
+
+    axios.post(url, data)
+      .then((response) => {
+        const message = response.data;
+
+        switch (message) {
+          case 'Registration Successfull':
+            alert('Registration successful!');
+            break;
+          case 'Erorr':
+            alert('User already exists. Try a different username.');
+            break;
+          default:
+            alert('Server response: ' + message);
+        }
+      })
+      .catch((error) => {
+        console.error('Registration error:', error);
+        alert(' An error occurred during registration.');
+      });
+  };
+
+  return (
+    <div className="Wrapper">
+      <form>
+        <h1>Create Account</h1>
+        <div className="Inputbox">
+          <input
+            value={firstname}
+            type="text"
+            placeholder="First Name*"
+            onChange={handleFirstnameChange}
+            required
+          />
         </div>
-    )
-}
+        <div className="Inputbox">
+          <input
+            value={lastname}
+            type="text"
+            placeholder="Last Name*"
+            onChange={handleLastnameChange}
+            required
+          />
+        </div>
+        <div className="Inputbox">
+          <input
+            value={username}
+            type="text"
+            placeholder="Email Address*"
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="Inputbox">
+          <input
+            value={password}
+            type="password"
+            placeholder="Password*"
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div className="Inputbox">
+          <input
+            value={phonenumbers}
+            type="text"
+            placeholder="Mobile Number*"
+            onChange={handlePhonenumbersChange}
+            required
+          />
+        </div>
+        <button onClick={handleOnClick}>Register</button>
+      </form>
+    </div>
+  );
+};
 export default CusRegisterForm;
