97e80e9ea90ae8bb63934d9f498d924868eac91e
Farhan Waggie <waggifarhan@gmail.com>
2025-05-24 23:53:35 +0200

    About us Complete

--- src/Components/About us.jsx
+++ src/Components/About us.jsx
@@ -1,34 +1,75 @@
 import React from "react";
-import { GoogleMaps} from "./Google maps/GoogleMaps.jsx";
+import {GoogleMaps} from "./Google maps/GoogleMaps.jsx";
 import '../Styles/About us.css';
 import myimage from '../assets/blank-profile-picture-973460_1280.webp';
 
 export function AboutUs() {
-  return (
-      <div>
-      <h1 className={"heading"}>About Us</h1>
-  <p className={"paragraph"}> At Sippin' Pretty, we believe every cup tells a story. Nestled in the heart of the community, our café is more than just a place to grab coffee—it's a cozy corner where great conversations, fresh flavors, and warm vibes come together. From expertly brewed coffees and handcrafted drinks to delicious bites made with love, we’re all about serving up comfort, quality, and a touch of charm. Whether you're here to catch up with friends, get some work done, or simply sip and unwind, you're always welcome at Sippin' Pretty.</p>
+    return (
+        <div>
+            <h1 className={"heading"}>About Us</h1>
+            <p className={"paragraph"}> At Sippin' Pretty, we believe every cup tells a story. Nestled in the heart of
+                the community, our café is more than just a place to grab coffee—it's a cozy corner where great
+                conversations, fresh flavors, and warm vibes come together. From expertly brewed coffees and handcrafted
+                drinks to delicious bites made with love, we’re all about serving up comfort, quality, and a touch of
+                charm. Whether you're here to catch up with friends, get some work done, or simply sip and unwind,
+                you're always welcome at Sippin' Pretty.</p>
 
-<h1 className={"heading"} >Find us here</h1>
-          <GoogleMaps />
-          <br />
-        <div className={"container"}>
-             <div className={"custom-card"}>
-                 <div className={"circle-wrapper"}>
-                     <img src={myimage}
-                          alt={"blank-profile-picture-973460_1280.webp"}
-                          className={"circle-image"} />
-                     <p>"My name is farhan. </p>
-                 </div>
-                 <div className={"custom-card"}>
-
-                 </div>
-
-
+            <h1 className={"heading"}>Find us here</h1>
+            <GoogleMaps/>
+            <br/>
+            <div>
+                <div>
+                    <div className={"container-wrapper"}>
+                        <div className={"container"}>
+                            <div className={"circle-wrapper"}>
+                                <img src={myimage}
+                                     alt={"blank-profile-picture-973460_1280.webp"}
+                                     className={"circle-image"}/>
+                            </div>
+                            <p className={"container-text"}>My name is Dylan</p>
+                        </div>
+                    </div>
                 </div>
+                <div>
+                    <div className={"container-wrapper"}>
+                        <div className={"container"}>
+                            <div className={"circle-wrapper"}>
+                                <img src={myimage}
+                                     alt={"blank-profile-picture-973460_1280.webp"}
+                                     className={"circle-image"}/>
+                            </div>
+                            <p className={"container-text"}>My name is Farhan</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={"container-wrapper"}>
+                        <div className={"container"}>
+                            <div className={"circle-wrapper"}>
+                                <img src={myimage}
+                                     alt={"blank-profile-picture-973460_1280.webp"}
+                                     className={"circle-image"}/>
+                            </div>
+                            <p className={"container-text"}>My name is Mulanga</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={"container-wrapper"}>
+                        <div className={"container"}>
+                            <div className={"circle-wrapper"}>
+                                <img src={myimage}
+                                     alt={"blank-profile-picture-973460_1280.webp"}
+                                     className={"circle-image"}/>
+                            </div>
+                            <p className={"container-text"}>My name is Sintu</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div> );
+    )
 
 }
-export default AboutUs;
+
 
