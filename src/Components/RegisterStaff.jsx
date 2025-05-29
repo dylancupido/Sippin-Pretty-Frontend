9b7c31984b7ec6d8aca97e1f345692117b0d5408
[dylancupido47@gmail.com] <dylancupido47@gmail.com>
2025-05-29 16:24:53 +0200

    removed 2 files and change api link

--- src/Components/RegisterStaff.jsx
+++ src/Components/RegisterStaff.jsx
@@ -6,11 +6,9 @@ import axios from "axios";
  
 const StaffRegisterForm = () => {
   const [name, setName] = useState('');
-  const [dob, setDOB] = useState(null); // use Date object
   const [username, setId] = useState('');
   const [password, setPassword] = useState('');
   const [phonenumber, setPhonenumber] = useState('');
-  const [address, setAddress] = useState('');
   const role = 'Staff';
  
   const handleOnClick = (event) => {
@@ -22,18 +20,16 @@ const StaffRegisterForm = () => {
       PhoneNumber: phonenumber,
       Password: password,
       Role: role,
-      Address: address,
-      DOB: dob ? dob.toISOString() : null,
  
     };
  
-    const url = 'http://localhost:5010/api/Staff/RegisterStaff';
+    const url = 'http://localhost:5010/api/Users/RegisterStaff';
  
     axios.post(url, data)
       .then((response) => {
         const message = response.data;
         switch (message) {
-          case 'Registration Successfull':
+          case 'Registration successful.':
             alert('Registration successful!');
             break;
           case 'Alreday Exist':
@@ -100,32 +96,7 @@ const StaffRegisterForm = () => {
             onChange={(e) => setPhonenumber(e.target.value)}
             required
           />
-        </div>
- 
-        <div className="Inputbox">
-          <DatePicker
-            selected={dob}
-            onChange={(date) => setDOB(date)}
-            dateFormat="yyyy-MM-dd"
-            placeholderText="Select date of birth*"
-            maxDate={new Date()}
-            showYearDropdown
-            scrollableYearDropdown
-            yearDropdownItemNumber={100}
-            required
-          />
-        </div>
- 
-        <div className="Inputbox">
-          <input
-            value={address}
-            type="text"
-            placeholder="Home Address*"
-            onChange={(e) => setAddress(e.target.value)}
-            required
-          />
-        </div>
- 
+        </div> 
         <button onClick={handleOnClick}>Register</button>
       </form>
     </div>
